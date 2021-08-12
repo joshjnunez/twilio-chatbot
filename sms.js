cef3d12f266e73f0055e4ebb16738aac943b429f
Joshua Nunez <joshuanunez@Joshuas-MacBook-Pro-2.local>
2021-08-12 16:10:17 -0500

    updated env variables

--- sms.js
+++ sms.js
@@ -1,13 +1,13 @@
 require('dotenv').config()
 
 
-const accountSid = 'AC1d15f792cbee3ea001f72cf1f8d998f4';
-const authToken = 'fde74b9b7f194a440d92bd479a9e92ce';
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 
 const client = require('twilio')(accountSid, authToken);
 
 client.messages.create({
-    to: '+15045121694',
+    to: process.env.MY_PHONE_NUMBER,
     from: '+19084598121',
     body: 'Hello world!',
 }).then((message)=> console.log(message.sid))
